b145eddbc53aa47b152a46fac092b61e10a8ef27
hiyankey <hiyankey@gmail.com>
2024-10-22 03:24:33 +0000

    build a screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
+import Button from '@/components/Button'
+import ImageViewer from '@/components/ImageViewer'
 import { Link } from 'expo-router'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
+const PlaceholderImage = require('@/assets/images/background-image.png')
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href={'/about'} style={styles.button}>
-        Go to about screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label='Choose a photo' />
+        <Button label='Use this photo' />
+      </View>
     </View>
   )
 }
@@ -16,17 +22,15 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 })
