c0eecdab826d58c80ffa6ef92c8a7fa1ce8f1a2a
hiyankey <hiyankey@gmail.com>
2024-10-22 00:39:38 +0000

    add nav

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,14 +1,10 @@
-import { Link } from 'expo-router'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function Index() {
+export default function About() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href={'/about'} style={styles.button}>
-        Go to about screen
-      </Link>
+      <Text style={styles.text}>About screen</Text>
     </View>
   )
 }
@@ -16,17 +12,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
     color: '#fff',
   },
 })
