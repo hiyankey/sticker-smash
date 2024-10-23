95f05fb5a20a91eb6a8d3ffa7245b8a0a2cd43fa
hiyankey <hiyankey@gmail.com>
2024-10-23 15:23:44 +0000

    create modal

---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,19 @@
+import { View } from 'react-native'
+import { Image } from 'expo-image'
+import React from 'react'
+
+type Props = {
+  imageSize: number
+  stickerSource: string
+}
+
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  return (
+    <View style={{ top: -350 }}>
+      <Image
+        source={stickerSource}
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  )
+}
